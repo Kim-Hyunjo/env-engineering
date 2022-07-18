ecc5a21f8ea08a191c67cd998a3c4a39c9f7e20f
kimhyunjo <alsxmqkqh23@gmail.com>
2022-07-18 20:08:45 +0900

    Add hello

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Sewege Treatment Solution</h1>
       </header>
+      <div>hello</div>
       <nav></nav>
       <Dashboard />
     </div>
